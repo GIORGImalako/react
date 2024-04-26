dbdfd47daa98220e7a6ecf935f6f04ad619af1cc
Giorgi Malakmadze <malakmadzegio5@gmail.com>
2024-04-26 17:45:57 +0400

    smoll fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import { BrowserRouter , Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./components/home/Home";
+import { Contact } from "./components/contact/Contact";
+import { About } from "./components/about/About";
+import { NoPage } from "./components/nopage/NoPage";
+import { Layout } from "./layout/Layout";
+
+
 
 function App() {
   return (
-    <>
-      Hello World!
-    </>
+  <>
+  
+  <Layout/>
+     <Routes>
+      <Route index element={<Home />}></Route>
+      <Route path="about" element={<About />}/>
+      <Route path="contact" element={<Contact />}/>
+      <Route path="*" element={<NoPage/>} />
+      </Routes>
+      </>
   );
 }
-
 export default App;
